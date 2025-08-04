a8b9b8477e268a8c32a94fdb7ccc152d4578a499
fedropoll <arstanbekovasil10@gmail.com>
2025-08-04 17:30:29 +0600

    исправил ошибку в списке врачей

--- manage.py
+++ manage.py
@@ -4,22 +4,25 @@ import os
 import sys
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safe.settings')
+
+    # Импорт и работа с Django должна быть здесь, после установки DJANGO_SETTINGS_MODULE
+    import django
+    django.setup()  # важный вызов — инициализирует всё для работы с ORM
+
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    # Создаем суперпользователя, если его нет
+    if not User.objects.filter(email="admin@example.com").exists():
+        User.objects.create_superuser(
+            email="admin@example.com",
+            password="admin1234",
+            username="admin"
+        )
+
     try:
         from django.core.management import execute_from_command_line
-
-        # Перенесённый сюда блок создания суперпользователя
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-
-        if not User.objects.filter(email="admin@example.com").exists():
-            User.objects.create_superuser(
-                email="admin@example.com",
-                password="admin1234",
-                username="admin"
-            )
-
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
