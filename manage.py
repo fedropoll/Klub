299c3acab4a7a1e95ee2bdf34ddcdba3f12a5062
fedropoll <arstanbekovasil10@gmail.com>
2025-08-04 17:27:48 +0600

    исправил ошибку в списке врачей

--- manage.py
+++ manage.py
@@ -3,30 +3,31 @@
 import os
 import sys
 
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
-if not User.objects.filter(email="admin@example.com").exists():
-    User.objects.create_superuser(
-        email="admin@example.com",
-        password="admin1234",
-        username="admin"
-    )
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safe.settings')
     try:
         from django.core.management import execute_from_command_line
+
+        # Перенесённый сюда блок создания суперпользователя
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if not User.objects.filter(email="admin@example.com").exists():
+            User.objects.create_superuser(
+                email="admin@example.com",
+                password="admin1234",
+                username="admin"
+            )
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
 
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()
-
