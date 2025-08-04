c7d7ef90d5c911f69bcb87d292f4b49014964c7e
fedropoll <arstanbekovasil10@gmail.com>
2025-08-04 17:23:48 +0600

    исправил ошибку в списке врачей

--- manage.py
+++ manage.py
@@ -3,6 +3,15 @@
 import os
 import sys
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(email="admin@example.com").exists():
+    User.objects.create_superuser(
+        email="admin@example.com",
+        password="admin1234",
+        username="admin"
+    )
 
 def main():
     """Run administrative tasks."""
