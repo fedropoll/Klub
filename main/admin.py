6a1550913d39e19bbb363c2a9b1ceab7c5f136be
fedropoll <arstanbekovasil10@gmail.com>
2025-08-05 14:10:21 +0600

    исправил ошибку связоный с деректором

--- main/admin.py
+++ main/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, UserProfile, ClientProfile, EmailVerificationCode
 
-try:
-    admin.site.unregister(CustomUser)
-except admin.sites.NotRegistered:
-    pass
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Профиль пользователя'
+    fk_name = 'user'
 
 @admin.register(CustomUser)
-class CustomUserAdmin(BaseUserAdmin):
-    model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+    list_display = ('email', 'is_staff', 'is_active', 'get_role')
+    list_select_related = ('user_profile',)
     ordering = ('email',)
     search_fields = ('email',)
+
     fieldsets = (
         (None, {'fields': ('email', 'password', 'username')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
@@ -21,25 +24,33 @@ class CustomUserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'username', 'is_staff', 'is_active'),
-        }),
+            'fields': ('email', 'password1', 'password2', 'username', 'is_staff', 'is_active')}
+        ),
     )
+
+    def get_role(self, obj):
+        return obj.user_profile.role if hasattr(obj, 'user_profile') else '—'
+    get_role.short_description = 'Роль'
+    get_role.admin_order_field = 'user_profile__role'
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'role')
     search_fields = ('user__email',)
 
+
 @admin.register(ClientProfile)
 class ClientProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'full_name', 'phone')
     search_fields = ('user__email', 'full_name', 'phone')
 
+
 @admin.register(EmailVerificationCode)
 class EmailVerificationCodeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'code', 'created_at', 'is_used', 'is_expired')
-    list_filter = ('is_used',)
-    search_fields = ('user__email', 'code')
+    list_display = ['user', 'code', 'created_at', 'is_used', 'is_expired']
+    list_filter = ['is_used']
+    search_fields = ['user__email', 'code']
 
     def is_expired(self, obj):
         return obj.is_expired()
