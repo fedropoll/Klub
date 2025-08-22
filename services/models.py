84910919f338990248851fc5bf712218616d7248
fedropoll <arstanbekovasil10@gmail.com>
2025-08-22 18:58:47 +0600

    Initial commit

--- services/models.py
+++ services/models.py
@@ -12,6 +12,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Service(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название услуги")
     description = models.TextField(verbose_name="Описание", blank=True)
