78b8a974133581aa51f3478535833308d8be4b90
fedropoll <arstanbekovasil10@gmail.com>
2025-08-26 19:47:51 +0600

    изменнение в докерах и настройках

--- safe/settings.py
+++ safe/settings.py
@@ -14,13 +14,14 @@ DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'safe_clinic'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', 5432),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
