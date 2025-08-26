6938e34cc35b524aa023d2532767eeaa1e42afa8
fedropoll <arstanbekovasil10@gmail.com>
2025-08-26 23:33:45 +0600

    Fix CORS for Swagger settings

--- safe/urls.py
+++ safe/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.http.response import HttpResponse
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework import permissions
+from rest_framework.permissions import AllowAny
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -15,7 +15,7 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="contact@safe.com"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=(AllowAny,),
 )
 
 def home(request):
@@ -34,8 +34,10 @@ urlpatterns = [
     path('analytics/', include('data_analytics.urls')),
     path('', include('main.urls')),
 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Swagger и Redoc
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
