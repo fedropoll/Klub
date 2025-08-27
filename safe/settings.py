fcfe7973960be059b47c1555c622997ce14b6cbc
fedropoll <arstanbekovasil10@gmail.com>
2025-08-27 17:39:24 +0600

    пропобовал через докер не получилос пришлос делат по другому

--- safe/settings.py
+++ safe/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['safeclinic-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 DATABASES = {
